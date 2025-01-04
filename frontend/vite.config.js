b69e5ac3cd2c7bf39b4eceac511664672ffb91b5
Darshan M P <mpdarshan204@gmail.com>
2025-01-04 21:31:09 +0530

    vite build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,9 @@ d.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Change the output directory to 'build'.
+  },
   server: {
     proxy: { '/foo': 'http://localhost:5173',
       '/api': {
