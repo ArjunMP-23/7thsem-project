adb9bec5eb2821099593284af436b7a00efbda8b
Darshan M P <mpdarshan204@gmail.com>
2025-02-11 20:44:58 +0530

    abs

--- frontend/src/website/aboutUs/AboutUs.jsx
+++ frontend/src/website/aboutUs/AboutUs.jsx
@@ -1,86 +1,79 @@
 import React from "react";
 import Hero from "../../components/website/hero/Hero";
-
+import logo1 from "../../assets/photo1.jpg";
+import logo2 from "../../assets/photo2.jpg";
+import logo3 from "../../assets/photo3.jpg";
+import logo4 from "../../assets/photo4.jpg";
+import "./AboutUS.scss";
 const AboutUs = () => {
   return (
+    <>
     <div className="bg text__color">
       <Hero
         title="Introduction"
         text={
-          <>
-            <p>
-              Libraries are vital hubs for academic growth and intellectual
-              engagement, offering more than just shelves of books. They are
-              dynamic spaces where individuals come together to study, write,
-              and collaborate. They provide an environment conducive to
-              scholarly research, enabling students, faculty, and researchers to
-              deepen their knowledge and explore new ideas.
-            </p>
-            <p>
-              In addition to physical books, modern libraries integrate
-              cutting-edge information technology, allowing users to access
-              digital resources, databases, and e-journals from anywhere. They
-              often provide training and workshops on research methodologies,
-              digital literacy, and the effective use of library tools,
-              empowering users to navigate the vast world of information with
-              confidence.
-            </p>
-            <p>
-              The College Library, in particular, serves as a gateway to
-              information and knowledge. It connects learners to an extensive
-              array of resources, including printed books, academic journals,
-              multimedia content, and online repositories. It supports
-              interdisciplinary learning and ensures that users stay updated
-              with the latest advancements in their respective fields.
-            </p>
-            <p>
-              At RUAS (Ramaiah University of Applied Sciences), the library
-              system is a cornerstone of academic excellence. It is designed to
-              cater to diverse learning needs with a well-curated collection of
-              printed and electronic resources. The libraries here foster a
-              culture of curiosity and continuous learning, equipping students
-              and researchers with the tools they need to succeed academically
-              and professionally.
-            </p>
-          </>
+          
+          
+          "Welcome to the About Us page of our Library Management System project! We are a team of passionate and dedicated students from M S Ramaiah University of Applied Science working together to build an innovative and user-friendly platform for managing library resources efficiently. Each of us brings unique skills and expertise to the table, ensuring that this project reflects our commitment to excellence. "
+      
         }
         reverse={true}
       />
-      <Hero
-        title="Team Information"
-        text={
-          <p>
-            <table border="0" >
-            <tbody>
-              <tr>
-                <th className="bg__primary"style={{color: "black"}} >Name</th>
-                <th className="bg__primary"style={{color: "black"}}>Registration Number</th>
-              </tr>
-             
-              <tr>
-                <td>Adarsh B Patil</td>
-                <td>21ETEC004003</td>
-              </tr>
-              <tr>
-                <td>Arjun M P</td>
-                <td>21ETEC004013</td>
-              </tr>
+      <footer className="bg__hover text__color">
+      <div className="top" >
+              <div className="card ">
+                <img className="card1 "src={logo2} alt="Avatar" style={{ width: "100%" }} />
+                <div className="">
+                  <h4>
+                    <b>Adarsh B Patil</b>
+                  </h4>
+                  <h4> <b>21ETEC004003</b></h4>
+                  
+                </div>
+              </div>
+            
+            
+              <div className="card">
+                <img className="card1 " src={logo3} alt="Avatar" style={{ width: "100%" }} />
+                <div className="">
+                  <h4>
+                    <b>Arjun M P</b>
+                    
+                  </h4>
+                  <h4> <b>21ETEC004013</b></h4>
+                </div>
+              </div>
+          
+            
+              <div className="card">
+                <img className="card1 " src={logo4} alt="Avatar" style={{ width: "100%" }} />
+                <div className="">
+                  <h4>
+                    <b>Mohammod Ibrahim Chickle</b>
+                   
+                  </h4>
+                  <h4> <b>21ETEC004003</b></h4>
+                </div>
+              </div>
+          
+           
+              <div className="card">
+                <img className="card1 " src={logo1} alt="Avatar" style={{ width: "100%" }} />
+                <div className="">
+                  <h4>
+                    <b>Vinod Kumar</b>
+                  </h4>
+                  <h4> <b>21ETEC004003</b></h4>
+                </div>
+              </div>
               
-              <tr>
-                <td>Mohammod Ibrahim Chickle &nbsp; </td>
-                <td>21ETEC004042</td>
-              </tr>
-              <tr>
-                <td>Vinod Kumar</td>
-                <td>21ETEC004078</td>
-              </tr>
-              </tbody>
-            </table>
-          </p>
-        }
-        reverse={false}
-      />
+          
+      </div>
+      </footer>
+     
     </div>
+
+    </>
   );
 };
 
